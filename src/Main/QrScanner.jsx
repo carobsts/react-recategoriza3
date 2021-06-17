ee2723b7182f70dd4fa0044e9878f3c9360b6e2f
carolinabsts <carolina.bustos@dinocloudconsulting.com>
2021-06-16 22:29:12 -0300

    save changes.

--- src/Main/QrScanner.jsx
+++ src/Main/QrScanner.jsx
@@ -1,27 +1,16 @@
-import { Button } from '@material-ui/core';
 import React, { useState } from 'react'
-import QrReader from 'react-qr-scanner'
+import BarcodeReader from 'react-barcode-reader';
 import { useHistory } from 'react-router';
-import { useDisplaySize } from '../hooks/useDisplaySize';
 import { paths } from '../Routes/paths';
-import SwapVertIcon from '@material-ui/icons/SwapVert';
 import './styles.css';
 
 const QrScannerComponent = () => {
 
     const history = useHistory();
-
-    const { widthSize, heightSize } = useDisplaySize();
     const [ result, setResult ] = useState();
-    const [ facingMode, setFacingMode ] = useState('user');
   
     function handleScan(data) {
        setResult(data);
-    };
-
-    const previewStyle = {
-        height: heightSize,
-        width: widthSize,
     };
 
     if (result) {
@@ -36,21 +25,20 @@ const QrScannerComponent = () => {
     return(
       <div>
         <div className="ButtonCameraFixed">
-          <Button 
+          {/* <Button 
           onClick={() => facingMode === 'environment' 
           ? setFacingMode('user') 
           : setFacingMode('environment')}
           variant="contained" color="primary"
           >
             <SwapVertIcon/>
-          </Button>
+          </Button> */}
         </div>
-        <QrReader
-          legacyMode={true}
-          facingMode={facingMode}
-          style={previewStyle}
+        <div>
+          <BarcodeReader
           onScan={handleScan}
-        />
+          />
+        </div>
       </div>
     )
 };
